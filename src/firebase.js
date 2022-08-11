2a133bb8025540811bfa6b3e870a0f45b45fe49f
omaran bazna <omran4d@gmail.com>
2022-08-11 17:33:43 -0400

    add firebase to the project

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOHXCWa2d6wlIH-66uRfEQIjMfS4ND_Sk",
+  authDomain: "disney-clone-f4aeb.firebaseapp.com",
+  projectId: "disney-clone-f4aeb",
+  storageBucket: "disney-clone-f4aeb.appspot.com",
+  messagingSenderId: "275486830296",
+  appId: "1:275486830296:web:c1e6a7c1e0be582686f1ad",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
