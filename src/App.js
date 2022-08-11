1b56edc7f1b8ec6083d29ded2bb04f1b74e6a60a
omaran bazna <omran4d@gmail.com>
2022-08-11 16:42:04 -0400

    import the header to the main app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import LogIn from "./components/LogIn";
+import Header from "./components/Header";
 import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<LogIn />} />
       </Routes>
