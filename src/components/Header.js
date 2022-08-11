ec59a7d6a13ff3b48a8200f679c9dc82bbe290b0
omaran bazna <omran4d@gmail.com>
2022-08-11 16:45:20 -0400

    create Nav component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Nav>Header</Nav>
+    </div>
+  );
 };
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+`;
 
 export default Header;
