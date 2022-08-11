728a7056566e7465ff5cd79977b31eada6274164
omaran bazna <omran4d@gmail.com>
2022-08-10 23:53:39 -0400

    add styled component

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -3,13 +3,28 @@ import styled from "styled-components";
 const LogIn = () => {
   return (
     <Container>
-      <Content>Content</Content>
+      <Content>content</Content>
     </Container>
   );
 };
 const Container = styled.section`
   display: flex;
   overflow: hidden;
+  flex-direction: column;
+  text-align: center;
 `;
-const Content = styled.div``;
+const Content = styled.div`
+  margin-bottom: 10px;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 80px 60px;
+  height: 100%;
+`;
+
 export default LogIn;
