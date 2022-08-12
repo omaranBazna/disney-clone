54504cc68fdd80083e22b320eeacb833f636bb10
omaran bazna <omran4d@gmail.com>
2022-08-12 10:33:39 -0400

    add some details to the details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -8,6 +8,12 @@ const Details = () => {
       <Background>
         <img src="" alt="" />
       </Background>
+      <ImageTitle>
+        <img src="" alt="" />
+      </ImageTitle>
+      <ContentMeta>
+        <Controls>controls</Controls>
+      </ContentMeta>
     </Container>
   );
 };
@@ -35,5 +41,30 @@ const Background = styled.div`
       width: initial;
     }
   }
+`;
+const ImageTitle = styled.div`
+  align-items: flex-end;
+  display: flex;
+  -webkit-box-pack: start;
+  justify-content: start;
+  margin: 0px auto;
+  height: 30vw;
+  min-height: 170px;
+  padding-bottom: 24px;
+  img {
+    max-width: 600px;
+    min-width: 200px;
+    width: 35vw;
+  }
+`;
+const ContentMeta = styled.div`
+  max-width: 874px;
+`;
+const Controls = styled.div`
+  align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  margin: 24px 0px;
+  min-height: 56px;
 `;
 export default Details;
