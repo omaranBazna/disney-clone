a21bad795a7c5a79b0d830ea882ad6f046c3c6dc
omaran bazna <omran4d@gmail.com>
2022-08-11 20:58:43 -0400

    styling the image slides

--- src/components/ImageSlides.js
+++ src/components/ImageSlides.js
@@ -73,14 +73,19 @@ const Wrap = styled.div`
   position: relative;
   a {
     border-radius: 4px;
-    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30 px -10px,
-      rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    box-shadow: rgba(0, 0, 0, 0.69) 0px 26px 30px -10px,
+      rgb(0, 0, 0, 0.73) 0px 16px 10px -10px;
     cursor: pointer;
     display: block;
     padding: 4px;
     img {
       width: 100%;
       height: 100%;
+    }
+    &:hover {
+      padding: 0;
+      border: 4px solid rgba(249, 249, 249, 0.8);
+      transition-duration: 300ms;
     }
   }
 `;
