e1b8f52e5179411c146aa5a3e18cabc956450908
omaran bazna <omran4d@gmail.com>
2022-08-12 00:03:50 -0400

    get data from firebase collections

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,11 +8,12 @@ import Originals from "./Originals";
 import Trending from "./Trending";
 
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux/es/hooks/useDispatch";
+import { useDispatch } from "react-redux/es/hooks/useDispatch";
+import { useSelector } from "react-redux/es/hooks/useSelector";
 import { db } from "../firebase";
 import { setMovies } from "../features/moviesSlice";
 import { selectUserName } from "../features/userSlice";
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, onSnapshot, doc, query, where } from "firebase/firestore";
 const Home = () => {
   const dispatch = useDispatch();
   const userName = useSelector(selectUserName);
@@ -20,16 +21,40 @@ const Home = () => {
   let newDisney = [];
   let originals = [];
   let trending = [];
+
   useEffect(() => {
-    db.collection("movies").onSnapshot((snapshot) => {
-      snapshot.docs.map((doc) => {
-        switch (doc.data().type) {
-          case "recommended":
-            recommends.push({ id: doc.id, ...doc.data() });
-        }
+    const q1 = query(
+      collection(db, "movies"),
+      where("type", "==", "recommend")
+    );
+    const unsubscribe1 = onSnapshot(q1, (querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        recommends.push({ id: doc.id, ...doc.data() });
+      });
+    });
+
+    const q2 = query(collection(db, "movies"), where("type", "==", "new"));
+    const unsubscribe2 = onSnapshot(q1, (querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        newDisney.push({ id: doc.id, ...doc.data() });
+      });
+    });
+
+    const q3 = query(collection(db, "movies"), where("type", "==", "trending"));
+    const unsubscribe3 = onSnapshot(q1, (querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        trending.push({ id: doc.id, ...doc.data() });
+      });
+    });
+
+    const q4 = query(collection(db, "movies"), where("type", "==", "original"));
+    const unsubscribe4 = onSnapshot(q1, (querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        originals.push({ id: doc.id, ...doc.data() });
       });
     });
   }, []);
+
   return (
     <Container>
       <ImageSlides />
