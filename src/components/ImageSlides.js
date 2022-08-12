b720d2b117af1a0eba18653a3d337182f4c88fbb
omaran bazna <omran4d@gmail.com>
2022-08-11 20:26:55 -0400

    configure the react slick slides setting

--- src/components/ImageSlides.js
+++ src/components/ImageSlides.js
@@ -1,6 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 const ImageSlides = () => {
+  let setting = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
   return <div></div>;
 };
 
