a6412f1ea27ff014b5a0a5c73158b3e549ad7a7e
omaran bazna <omran4d@gmail.com>
2022-08-12 10:20:15 -0400

    add details to the main app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import LogIn from "./components/LogIn";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import { Routes, Route } from "react-router-dom";
+import Details from "./components/Details";
 function App() {
   return (
     <div>
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LogIn />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/movies/:id" element={<Details />} />
       </Routes>
     </div>
   );
