9759b40413729428cc21b0bb5f41ded0009659ea
omaran bazna <omran4d@gmail.com>
2022-08-11 22:52:29 -0400

    styling the recommendations

--- src/components/Recommendations.js
+++ src/components/Recommendations.js
@@ -1,14 +1,15 @@
 import styled from "styled-components";
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Recommendations = () => {
   return (
     <Container>
       <Content>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
+        <Wrap>
+          <Link to="/">
+            <img src="https://www.lanc.org.uk/wp-content/uploads/2011/07/ASD.png" />
+          </Link>
+        </Wrap>
       </Content>
     </Container>
   );
@@ -25,5 +26,11 @@ const Content = styled.div`
   @media (max-width: 768px) {
     grid-template-columns: repeat(2, minmax(0, 1fr));
   }
+`;
+const Wrap = styled.div`
+  padding-top: 56.25%;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.69) 0px 26px 30px -10px,
+    rgba(0, 0, 0, 0.73) 0px 16px 10px -10px;
 `;
 export default Recommendations;
