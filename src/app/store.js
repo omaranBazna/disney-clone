94d0eb0c8792110b5c0d31ec46dfb7952e9191f3
omaran bazna <omran4d@gmail.com>
2022-08-11 23:25:38 -0400

    add movies reducer to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
 import { userReducer } from "../features/userSlice";
+import { moviesReducer } from "../features/userMovies";
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    movies: moviesReducer,
   },
   middleware: getDefaultMiddleware({
     serializableCheck: false,
